fc017e002e6ec8331712d0f13848f9ba89c07ffa
roussiba <roussibadiakhate@hotmail.fr>
2018-01-29 12:00:34 +0000

    Quelques modifications de messages

--- src/main/java/qualshore/livindkr/main/configSecurity/SecurityConstant.java
+++ src/main/java/qualshore/livindkr/main/configSecurity/SecurityConstant.java
@@ -19,7 +19,10 @@ public class SecurityConstant {
     public static final String IMG_PNG = "image/png";
     public static final String TOKEN = "Token invalide";
     public static final String EREREUR_EMAIL = "Nous n'avons pas pu vous envoyez le mail de validation. Veuillez saisir votre mail d'incription sur le formulaire suivant.";
+    public static final String EREREUR_EMAIL1 = "Nous n'avons pas pu vous envoyez le mail de validation. Veuillez reesayer la modification.";
+    public static final String SEND_EMAIL = "Nous vous avons envoyez le mail de validation.";
     public static final String INSCRIPTIONMSM = "Votre inscription est prise en compte. Veuillez valider le mail pour finaliser l'inscription !";
     public static final String VIDE_USER = "Veuillez renseigner les donneés de l'utilisateur !";
+    public static final String MSG_MOTDEPASSE= "Votre mot de passé a étè modifié avec succé !";
 }
 
