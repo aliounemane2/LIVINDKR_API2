032ea8b728acf6ac18df9b0e522e5342382488d3
Alioune <alioune.mane@qualshore.com>
2018-02-22 10:42:17 +0000

    Commit du 22 Fevrier 2018 a 10h 42

---
+++ src/main/java/qualshore/livindkr/main/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package qualshore.livindkr.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import qualshore.livindkr.main.entities.Menu;
+
+public interface MenuRepository extends JpaRepository<Menu, Integer>{
+
+}
